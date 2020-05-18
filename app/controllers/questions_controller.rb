20f321741a458d7342791dddb1a8f1ca15ab6fcc
Sven Kusebauch <sven.kusebauch@gmail.com>
2020-05-18 12:38:07 +0200

    implement coach answer logic

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -4,5 +4,12 @@ class QuestionsController < ApplicationController
 
   def answer
     @search_query = params[:question]
+    @search_result = if @search_query == "I am going to work"
+      "Great!"
+    elsif @search_query.include?("?")
+      "Silly question, get dressed and go to work!"
+    else
+      "I don't care, get dressed and go to work"
+    end
   end
 end
