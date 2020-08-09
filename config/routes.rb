61a1f94c4e466c6234cdadf5c33bf3fce420681d
Sven Kusebauch <sven.kusebauch@gmail.com>
2020-08-09 13:51:30 +0200

    add comment to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  # verb 'path', to: 'controller#action'
+  # verb 'path', to: 'controller#action' as: :ask (if you want to change prefix)
   # root to: "controller#action" - only get
   root to: 'questions#ask'
   get 'answer', to: 'questions#answer'
