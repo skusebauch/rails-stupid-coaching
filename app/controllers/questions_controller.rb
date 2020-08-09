268daf2e62d7cc60b482ce5969901e54dfb248b1
Sven Kusebauch <sven.kusebauch@gmail.com>
2020-08-09 13:49:18 +0200

    changed routes and minor changes at view controller

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -4,12 +4,15 @@ class QuestionsController < ApplicationController
 
   def answer
     @search_query = params[:question]
-    @search_result = if @search_query == "I am going to work"
-      "Great!"
-    elsif @search_query.include?("?")
-      "Silly question, get dressed and go to work!"
-    else
-      "I don't care, get dressed and go to work"
-    end
+    @search_result =
+      if @search_query == "I am going to work"
+        "Great!"
+      elsif @search_query.ends_with?("?")
+        "Silly question, get dressed and go to work!"
+      elsif @search_query.blank?
+        "I can't hear you!"
+      else
+       "I don't care, get dressed and go to work"
+      end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  # get ask from question controller
-  get 'ask', to: 'questions#ask'
-  # get answer form question controller
+  # verb 'path', to: 'controller#action'
+  # root to: "controller#action" - only get
+  root to: 'questions#ask'
   get 'answer', to: 'questions#answer'
 end
